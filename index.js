a2ad2f47a985df09272ea0df8c6ddebe930c1020
Vishwas Singh <singh.vishu234@gmail.com>
2020-10-24 19:36:23 +0530

    [Fix]:scroll+footer

--- index.js
+++ index.js
@@ -10,15 +10,15 @@
             if (document.body.scrollTop > 640 || 
                 document.documentElement.scrollTop > 640) //when small
             { 
-                document.querySelector(".main-head").style.opacity = "0.1"; //for less 0.1
-                document.querySelector(".lc1").style.top = "-500px";
+                // document.querySelector(".main-head").style.opacity = "0.1";
+                document.querySelector(".lc1").style.top = "-550px";
                 document.querySelector(".side-works").style.top = "-210px";
 
 
                     
             } 
             else {                                          //when large
-                document.querySelector(".main-head").style.opacity = "1";
+                // document.querySelector(".main-head").style.opacity = "1";
                 document.querySelector(".lc1").style.top = "-120px";
                 document.querySelector(".side-works").style.top = "0px";
 
@@ -30,14 +30,18 @@
             if (document.body.scrollTop > 1430 || 
                 document.documentElement.scrollTop > 1430) //when small
             { 
-                document.querySelector(".main-head2").style.opacity = "0.1",
+                // document.querySelector(".main-head2").style.opacity = "0.1",
+                // document.querySelector(".main-head2").style.position = "fixed"; 
+                // document.querySelector(".main-head2").style.top = "200px"; 
                 document.querySelector(".lc2").style.top = "-500px";
                 document.querySelector(".about").style.top = "-370px";
 
                     
             } 
             else {                                          //when large
-                document.querySelector(".main-head2").style.opacity = "1"; 
+                // document.querySelector(".main-head2").style.opacity = "1";
+                // document.querySelector(".main-head2").style.position = "relative";
+                // document.querySelector(".main-head2").style.top = "200px";
                 document.querySelector(".lc2").style.top = "0px"; 
                 document.querySelector(".about").style.top = "-100px";
             } 
@@ -47,18 +51,21 @@
             if (document.body.scrollTop > 2000 || 
                 document.documentElement.scrollTop > 2000) //when small
             { 
-                document.querySelector(".main-head3").style.fontSize = "5vw",
-                document.querySelector(".main-head3").style.lineHeight="100%";
-                document.querySelector(".main-head3").style.paddingLeft="70px";
-                document.querySelector(".main-head3").style.webkitTextStroke="2px #cccccc";
+                // document.querySelector(".main-head3").style.fontSize = "5vw",
+                // document.querySelector(".main-head3").style.lineHeight="100%";
+                // document.querySelector(".main-head3").style.paddingLeft="70px";
+                // document.querySelector(".main-head3").style.webkitTextStroke="2px #cccccc";
+                document.querySelector(".main-head2").style.opacity = "0"; 
+
                     
             } 
             else {                                          //when large
-                document.querySelector(".main-head3").style.fontSize = "20vw"; 
-                document.querySelector(".main-head3").style.lineHeight="90%";
-                document.querySelector(".main-head3").style.paddingLeft="0px";
-                document.querySelector(".main-head3").style.webkitTextStroke="6px #cccccc";          
+                // document.querySelector(".main-head3").style.fontSize = "20vw"; 
+                // document.querySelector(".main-head3").style.lineHeight="90%";
+                // document.querySelector(".main-head3").style.paddingLeft="0px";
+                // document.querySelector(".main-head3").style.webkitTextStroke="6px #cccccc"; 
+                document.querySelector(".main-head2").style.opacity = "0.1"; 
+         
             } 
         } 
-
 
