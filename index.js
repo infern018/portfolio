2a64ad2c6ee55f174ee1e8131e2f3bb628521f25
Vishwas Singh <singh.vishu234@gmail.com>
2020-10-27 19:51:36 +0530

    change(2)

--- index.js
+++ index.js
@@ -69,3 +69,37 @@
         //     } 
         // } 
 
+        function copyText(element) {
+            var range, selection;
+          
+            if (document.body.createTextRange) {
+              range = document.body.createTextRange();
+              range.moveToElementText(element);
+              range.select();
+            } else if (window.getSelection) {
+              selection = window.getSelection();        
+              range = document.createRange();
+              range.selectNodeContents(element);
+              selection.removeAllRanges();
+              selection.addRange(range);
+            }
+            
+            try {
+              document.execCommand('copy');
+              alert('text copied');
+            }
+            catch (err) {
+              alert('unable to copy text');
+            }
+          }
+
+          $(document).ready(function() {
+            //Preloader
+                preloaderFadeOutTime = 500;
+                function hidePreloader() {
+                var preloader = $('.body1');
+                preloader.fadeOut(preloaderFadeOutTime);
+            }
+            hidePreloader();
+        });
+
